9fee69fee317d2afbdb0d8a8a586c767156849e5
Allen Qiu <aqiu384@gmail.com>
2017-03-27 22:02:26 -0700

    same-arcana fusions

--- p5-tool/js/normal_recipes.js
+++ p5-tool/js/normal_recipes.js
@@ -1,817 +1,228 @@
 _NORMAL_RECIPES = {
-    "Abaddon": {
-        "arcana": "Judgement",
-        "max": 74,
-        "min": 64
-    },
-    "Agathion": {
-        "arcana": "Chariot",
-        "max": 3,
-        "min": 1
-    },
-    "Ame no Uzume": {
-        "arcana": "Lovers",
-        "max": 29,
-        "min": 19
-    },
-    "Ananta": {
-        "arcana": "Star",
-        "max": 43,
-        "min": 36
-    },
-    "Andras": {
-        "arcana": "Devil",
-        "max": 10,
-        "min": 5
-    },
-    "Angel": {
-        "arcana": "Justice",
-        "max": 12,
-        "min": 1
-    },
-    "Anubis": {
-        "arcana": "Judgement",
-        "max": 37,
-        "min": 1
-    },
-    "Anzu": {
-        "arcana": "Hierophant",
-        "max": 25,
-        "min": 22
-    },
-    "Apsaras": {
-        "arcana": "Priestess",
-        "max": 11,
-        "min": 6
-    },
-    "Ara Mitama": {
-        "arcana": "Chariot",
-        "max": 31,
-        "min": 25
-    },
-    "Arahabaki": {
-        "arcana": "Hermit",
-        "max": 35,
-        "min": 24
-    },
-    "Archangel": {
-        "arcana": "Justice",
-        "max": 16,
-        "min": 12
-    },
-    "Arsène": {
-        "arcana": "Fool",
-        "max": 1,
-        "min": 1
-    },
-    "Atropos": {
-        "arcana": "Fortune",
-        "max": 39,
-        "min": 34
-    },
-    "Attis": {
-        "arcana": "Hanged Man",
-        "max": 99,
-        "min": 60
-    },
-    "Baal": {
-        "arcana": "Emperor",
-        "max": 75,
-        "min": 66
-    },
-    "Baphomet": {
-        "arcana": "Devil",
-        "max": 58,
-        "min": 43
-    },
-    "Barong": {
-        "arcana": "Emperor",
-        "max": 52,
-        "min": 44
-    },
-    "Beelzebub": {
-        "arcana": "Devil",
-        "max": 99,
-        "min": 68
-    },
-    "Belial": {
-        "arcana": "Devil",
-        "max": 68,
-        "min": 62
-    },
-    "Belphegor": {
-        "arcana": "Tower",
-        "max": 37,
-        "min": 32
-    },
-    "Berith": {
-        "arcana": "Hierophant",
-        "max": 9,
-        "min": 1
-    },
-    "Bicorn": {
-        "arcana": "Hermit",
-        "max": 4,
-        "min": 1
-    },
-    "Bishamonten": {
-        "arcana": "Hierophant",
-        "max": 99,
-        "min": 63
-    },
-    "Black Ooze": {
-        "arcana": "Moon",
-        "max": 29,
-        "min": 24
-    },
-    "Black Rider": {
-        "arcana": "Tower",
-        "max": 59,
-        "min": 41
-    },
-    "Byakko": {
-        "arcana": "Temperance",
-        "max": 49,
-        "min": 33
-    },
-    "Cerberus": {
-        "arcana": "Chariot",
-        "max": 55,
-        "min": 39
-    },
-    "Chernobog": {
-        "arcana": "Death",
-        "max": 62,
-        "min": 53
-    },
-    "Choronzon": {
-        "arcana": "Magician",
-        "max": 28,
-        "min": 24
-    },
-    "Clotho": {
-        "arcana": "Fortune",
-        "max": 26,
-        "min": 1
-    },
-    "Cu Chulainn": {
-        "arcana": "Star",
-        "max": 99,
-        "min": 64
-    },
-    "Cybele": {
-        "arcana": "Priestess",
-        "max": 99,
-        "min": 55
-    },
-    "Daisoujou": {
-        "arcana": "Hierophant",
-        "max": 42,
-        "min": 39
-    },
-    "Dakini": {
-        "arcana": "Empress",
-        "max": 50,
-        "min": 40
-    },
-    "Decarabia": {
-        "arcana": "Fool",
-        "max": 32,
-        "min": 16
-    },
-    "Dionysus": {
-        "arcana": "Fool",
-        "max": 61,
-        "min": 42
-    },
-    "Dominion": {
-        "arcana": "Justice",
-        "max": 68,
-        "min": 60
-    },
-    "Eligor": {
-        "arcana": "Emperor",
-        "max": 16,
-        "min": 1
-    },
-    "Feng Huang": {
-        "arcana": "Hierophant",
-        "max": 22,
-        "min": 17
-    },
-    "Forneus": {
-        "arcana": "Hierophant",
-        "max": 63,
-        "min": 42
-    },
-    "Fortuna": {
-        "arcana": "Fortune",
-        "max": 46,
-        "min": 39
-    },
-    "Futsunushi": {
-        "arcana": "Magician",
-        "max": 99,
-        "min": 59
-    },
-    "Fuu-Ki": {
-        "arcana": "Star",
-        "max": 23,
-        "min": 11
-    },
-    "Gabriel": {
-        "arcana": "Temperance",
-        "max": 99,
-        "min": 62
-    },
-    "Ganesha": {
-        "arcana": "Sun",
-        "max": 53,
-        "min": 49
-    },
-    "Garuda": {
-        "arcana": "Star",
-        "max": 52,
-        "min": 43
-    },
-    "Genbu": {
-        "arcana": "Temperance",
-        "max": 7,
-        "min": 1
-    },
-    "Girimehkala": {
-        "arcana": "Moon",
-        "max": 44,
-        "min": 33
-    },
-    "Hanuman": {
-        "arcana": "Star",
-        "max": 64,
-        "min": 52
-    },
-    "Hariti": {
-        "arcana": "Empress",
-        "max": 40,
-        "min": 26
-    },
-    "Hecatoncheir": {
-        "arcana": "Hanged Man",
-        "max": 42,
-        "min": 29
-    },
-    "Hell Biker": {
-        "arcana": "Death",
-        "max": 39,
-        "min": 29
-    },
-    "High Pixie": {
-        "arcana": "Fool",
-        "max": 16,
-        "min": 8
-    },
-    "Horus": {
-        "arcana": "Sun",
-        "max": 49,
-        "min": 42
-    },
-    "Hua Po": {
-        "arcana": "Hanged Man",
-        "max": 9,
-        "min": 1
-    },
-    "Incubus": {
-        "arcana": "Devil",
-        "max": 5,
-        "min": 1
-    },
-    "Inugami": {
-        "arcana": "Hanged Man",
-        "max": 14,
-        "min": 9
-    },
-    "Ippon-Datara": {
-        "arcana": "Hermit",
-        "max": 13,
-        "min": 9
-    },
-    "Ishtar": {
-        "arcana": "Lovers",
-        "max": 99,
-        "min": 78
-    },
-    "Isis": {
-        "arcana": "Priestess",
-        "max": 26,
-        "min": 11
-    },
-    "Jack Frost": {
-        "arcana": "Magician",
-        "max": 11,
-        "min": 2
-    },
-    "Jack O'Lantern": {
-        "arcana": "Magician",
-        "max": 2,
-        "min": 1
-    },
-    "Jatayu": {
-        "arcana": "Tower",
-        "max": 32,
-        "min": 1
-    },
-    "Jikokuten": {
-        "arcana": "Temperance",
-        "max": 25,
-        "min": 20
-    },
-    "Kaiwan": {
-        "arcana": "Star",
-        "max": 36,
-        "min": 23
-    },
-    "Kali": {
-        "arcana": "Empress",
-        "max": 77,
-        "min": 56
-    },
-    "Kelpie": {
-        "arcana": "Strength",
-        "max": 6,
-        "min": 1
-    },
-    "Kikuri-Hime": {
-        "arcana": "Priestess",
-        "max": 40,
-        "min": 26
-    },
-    "Kin-Ki": {
-        "arcana": "Chariot",
-        "max": 25,
-        "min": 21
-    },
-    "King Frost": {
-        "arcana": "Emperor",
-        "max": 61,
-        "min": 52
-    },
-    "Kodama": {
-        "arcana": "Star",
-        "max": 11,
-        "min": 1
-    },
-    "Koppa Tengu": {
-        "arcana": "Temperance",
-        "max": 11,
-        "min": 7
-    },
-    "Koropokkuru": {
-        "arcana": "Hermit",
-        "max": 9,
-        "min": 4
-    },
-    "Koumokuten": {
-        "arcana": "Hermit",
-        "max": 49,
-        "min": 42
-    },
-    "Kumbhanda": {
-        "arcana": "Hermit",
-        "max": 42,
-        "min": 35
-    },
-    "Kurama Tengu": {
-        "arcana": "Hermit",
-        "max": 99,
-        "min": 49
-    },
-    "Kushinada-Hime": {
-        "arcana": "Lovers",
-        "max": 42,
-        "min": 29
-    },
-    "Kusi Mitama": {
-        "arcana": "Strength",
-        "max": 14,
-        "min": 6
-    },
-    "Lachesis": {
-        "arcana": "Fortune",
-        "max": 34,
-        "min": 26
-    },
-    "Lakshmi": {
-        "arcana": "Fortune",
-        "max": 99,
-        "min": 52
-    },
-    "Lamia": {
-        "arcana": "Empress",
-        "max": 26,
-        "min": 20
-    },
-    "Leanan Sidhe": {
-        "arcana": "Lovers",
-        "max": 19,
-        "min": 6
-    },
-    "Legion": {
-        "arcana": "Fool",
-        "max": 38,
-        "min": 32
-    },
-    "Lilim": {
-        "arcana": "Devil",
-        "max": 32,
-        "min": 10
-    },
-    "Lilith": {
-        "arcana": "Moon",
-        "max": 60,
-        "min": 44
-    },
-    "Mada": {
-        "arcana": "Tower",
-        "max": 99,
-        "min": 73
-    },
-    "Makami": {
-        "arcana": "Temperance",
-        "max": 15,
-        "min": 11
-    },
-    "Mandrake": {
-        "arcana": "Death",
-        "max": 3,
-        "min": 1
-    },
-    "Mara": {
-        "arcana": "Tower",
-        "max": 73,
-        "min": 59
-    },
-    "Matador": {
-        "arcana": "Death",
-        "max": 19,
-        "min": 9
-    },
-    "Melchizedek": {
-        "arcana": "Justice",
-        "max": 60,
-        "min": 41
-    },
-    "Mithra": {
-        "arcana": "Temperance",
-        "max": 33,
-        "min": 25
-    },
-    "Mithras": {
-        "arcana": "Sun",
-        "max": 39,
-        "min": 19
-    },
-    "Mokoi": {
-        "arcana": "Death",
-        "max": 9,
-        "min": 3
-    },
-    "Moloch": {
-        "arcana": "Hanged Man",
-        "max": 60,
-        "min": 42
-    },
-    "Mot": {
-        "arcana": "Death",
-        "max": 99,
-        "min": 62
-    },
-    "Mother Harlot": {
-        "arcana": "Empress",
-        "max": 99,
-        "min": 77
-    },
-    "Mothman": {
-        "arcana": "Moon",
-        "max": 33,
-        "min": 29
-    },
-    "Naga": {
-        "arcana": "Hermit",
-        "max": 24,
-        "min": 17
-    },
-    "Narcissus": {
-        "arcana": "Lovers",
-        "max": 50,
-        "min": 42
-    },
-    "Nebiros": {
-        "arcana": "Devil",
-        "max": 62,
-        "min": 58
-    },
-    "Nekomata": {
-        "arcana": "Magician",
-        "max": 17,
-        "min": 11
-    },
-    "Nigi Mitama": {
-        "arcana": "Temperance",
-        "max": 20,
-        "min": 15
-    },
-    "Norn": {
-        "arcana": "Fortune",
-        "max": 52,
-        "min": 46
-    },
-    "Nue": {
-        "arcana": "Moon",
-        "max": 20,
-        "min": 12
-    },
-    "Obariyon": {
-        "arcana": "Fool",
-        "max": 8,
-        "min": 1
-    },
-    "Oberon": {
-        "arcana": "Emperor",
-        "max": 66,
-        "min": 61
-    },
-    "Odin": {
-        "arcana": "Emperor",
-        "max": 99,
-        "min": 75
-    },
-    "Okuninushi": {
-        "arcana": "Emperor",
-        "max": 44,
-        "min": 36
-    },
-    "Oni": {
-        "arcana": "Strength",
-        "max": 19,
-        "min": 14
-    },
-    "Onmoraki": {
-        "arcana": "Moon",
-        "max": 12,
-        "min": 7
-    },
-    "Orobas": {
-        "arcana": "Hierophant",
-        "max": 17,
-        "min": 9
-    },
-    "Orthrus": {
-        "arcana": "Hanged Man",
-        "max": 21,
-        "min": 14
-    },
-    "Ose": {
-        "arcana": "Fool",
-        "max": 42,
-        "min": 38
-    },
-    "Pale Rider": {
-        "arcana": "Death",
-        "max": 53,
-        "min": 39
-    },
-    "Parvati": {
-        "arcana": "Lovers",
-        "max": 56,
-        "min": 50
-    },
-    "Pazuzu": {
-        "arcana": "Devil",
-        "max": 43,
-        "min": 32
-    },
-    "Pisaca": {
-        "arcana": "Death",
-        "max": 29,
-        "min": 19
-    },
-    "Pixie": {
-        "arcana": "Lovers",
-        "max": 2,
-        "min": 1
-    },
-    "Power": {
-        "arcana": "Justice",
-        "max": 41,
-        "min": 29
-    },
-    "Principality": {
-        "arcana": "Justice",
-        "max": 29,
-        "min": 16
-    },
-    "Queen Mab": {
-        "arcana": "Magician",
-        "max": 43,
-        "min": 28
-    },
-    "Quetzalcoatl": {
-        "arcana": "Sun",
-        "max": 99,
-        "min": 57
-    },
-    "Raja Naga": {
-        "arcana": "Temperance",
-        "max": 55,
-        "min": 49
-    },
-    "Rakshasa": {
-        "arcana": "Strength",
-        "max": 24,
-        "min": 19
-    },
-    "Rangda": {
-        "arcana": "Magician",
-        "max": 48,
-        "min": 43
-    },
-    "Raphael": {
-        "arcana": "Lovers",
-        "max": 78,
-        "min": 56
-    },
-    "Red Rider": {
-        "arcana": "Tower",
-        "max": 41,
-        "min": 37
-    },
-    "Saki Mitama": {
-        "arcana": "Lovers",
-        "max": 6,
-        "min": 2
-    },
-    "Sandalphon": {
-        "arcana": "Moon",
-        "max": 99,
-        "min": 60
-    },
-    "Sandman": {
-        "arcana": "Magician",
-        "max": 24,
-        "min": 17
-    },
-    "Sarasvati": {
-        "arcana": "Priestess",
-        "max": 50,
-        "min": 45
-    },
-    "Satan": {
-        "arcana": "Judgement",
-        "max": 99,
-        "min": 74
-    },
-    "Scathach": {
-        "arcana": "Priestess",
-        "max": 45,
-        "min": 40
-    },
-    "Seiryu": {
-        "arcana": "Temperance",
-        "max": 62,
-        "min": 55
-    },
-    "Setanta": {
-        "arcana": "Emperor",
-        "max": 28,
-        "min": 16
-    },
-    "Shiisaa": {
-        "arcana": "Chariot",
-        "max": 16,
-        "min": 10
-    },
-    "Shiki-Ouji": {
-        "arcana": "Chariot",
-        "max": 21,
-        "min": 16
-    },
-    "Siegfried": {
-        "arcana": "Strength",
-        "max": 69,
-        "min": 44
-    },
-    "Silky": {
-        "arcana": "Priestess",
-        "max": 6,
-        "min": 1
-    },
-    "Skadi": {
-        "arcana": "Priestess",
-        "max": 55,
-        "min": 50
-    },
-    "Slime": {
-        "arcana": "Chariot",
-        "max": 10,
-        "min": 3
-    },
-    "Succubus": {
-        "arcana": "Moon",
-        "max": 7,
-        "min": 1
-    },
-    "Sudama": {
-        "arcana": "Hermit",
-        "max": 17,
-        "min": 13
-    },
-    "Sui-Ki": {
-        "arcana": "Moon",
-        "max": 24,
-        "min": 20
-    },
-    "Surt": {
-        "arcana": "Magician",
-        "max": 59,
-        "min": 48
-    },
-    "Suzaku": {
-        "arcana": "Sun",
-        "max": 19,
-        "min": 1
-    },
-    "Take-Minakata": {
-        "arcana": "Hanged Man",
-        "max": 29,
-        "min": 21
-    },
-    "Thor": {
-        "arcana": "Chariot",
-        "max": 99,
-        "min": 55
-    },
-    "Thoth": {
-        "arcana": "Emperor",
-        "max": 36,
-        "min": 28
-    },
-    "Titania": {
-        "arcana": "Empress",
-        "max": 56,
-        "min": 50
-    },
-    "Unicorn": {
-        "arcana": "Hierophant",
-        "max": 39,
-        "min": 25
-    },
-    "Uriel": {
-        "arcana": "Justice",
-        "max": 99,
-        "min": 68
-    },
-    "Valkyrie": {
-        "arcana": "Strength",
-        "max": 44,
-        "min": 31
-    },
-    "Vishnu": {
-        "arcana": "Fool",
-        "max": 99,
-        "min": 61
-    },
-    "White Rider": {
-        "arcana": "Chariot",
-        "max": 39,
-        "min": 31
-    },
-    "Yaksini": {
-        "arcana": "Empress",
-        "max": 20,
-        "min": 1
-    },
-    "Yamata no Orochi": {
-        "arcana": "Judgement",
-        "max": 64,
-        "min": 37
-    },
-    "Yatagarasu": {
-        "arcana": "Sun",
-        "max": 57,
-        "min": 53
-    },
-    "Yurlungur": {
-        "arcana": "Sun",
-        "max": 42,
-        "min": 39
-    },
-    "Zaou-Gongen": {
-        "arcana": "Strength",
-        "max": 99,
-        "min": 69
-    },
-    "Zouchouten": {
-        "arcana": "Strength",
-        "max": 31,
-        "min": 24
-    }
+    "Chariot": [
+        0,
+        3,
+        10,
+        16,
+        21,
+        25,
+        31,
+        39,
+        55,
+        64
+    ],
+    "Death": [
+        0,
+        3,
+        9,
+        19,
+        29,
+        39,
+        53,
+        62,
+        72
+    ],
+    "Devil": [
+        0,
+        5,
+        10,
+        32,
+        43,
+        58,
+        62,
+        68,
+        84
+    ],
+    "Emperor": [
+        0,
+        16,
+        28,
+        36,
+        44,
+        52,
+        61,
+        66,
+        75,
+        82
+    ],
+    "Empress": [
+        0,
+        20,
+        26,
+        40,
+        50,
+        56,
+        77,
+        80
+    ],
+    "Fool": [
+        0,
+        1,
+        8,
+        16,
+        32,
+        38,
+        42,
+        61,
+        83
+    ],
+    "Fortune": [
+        0,
+        26,
+        34,
+        39,
+        46,
+        52,
+        69
+    ],
+    "Hanged Man": [
+        0,
+        9,
+        14,
+        21,
+        29,
+        42,
+        60,
+        82
+    ],
+    "Hermit": [
+        0,
+        4,
+        9,
+        13,
+        17,
+        24,
+        35,
+        42,
+        49,
+        56
+    ],
+    "Hierophant": [
+        0,
+        9,
+        17,
+        22,
+        25,
+        39,
+        42,
+        63,
+        67
+    ],
+    "Judgement": [
+        0,
+        37,
+        64,
+        74,
+        92
+    ],
+    "Justice": [
+        0,
+        12,
+        16,
+        29,
+        41,
+        60,
+        68,
+        81
+    ],
+    "Lovers": [
+        0,
+        2,
+        6,
+        19,
+        29,
+        42,
+        50,
+        56,
+        78,
+        85
+    ],
+    "Magician": [
+        0,
+        2,
+        11,
+        17,
+        24,
+        28,
+        43,
+        48,
+        59,
+        76
+    ],
+    "Moon": [
+        0,
+        7,
+        12,
+        20,
+        24,
+        29,
+        33,
+        44,
+        60,
+        75
+    ],
+    "Priestess": [
+        0,
+        6,
+        11,
+        26,
+        40,
+        45,
+        50,
+        55,
+        73
+    ],
+    "Star": [
+        0,
+        11,
+        23,
+        36,
+        43,
+        52,
+        64,
+        67
+    ],
+    "Strength": [
+        0,
+        6,
+        14,
+        19,
+        24,
+        31,
+        44,
+        69,
+        80
+    ],
+    "Sun": [
+        0,
+        19,
+        39,
+        42,
+        49,
+        53,
+        57,
+        63
+    ],
+    "Temperance": [
+        0,
+        7,
+        11,
+        15,
+        20,
+        25,
+        33,
+        49,
+        55,
+        62,
+        77
+    ],
+    "Tower": [
+        0,
+        32,
+        37,
+        41,
+        59,
+        73,
+        85
+    ]
 }
